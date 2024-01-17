af0b20a7c49345c651ef9daaff3bca7b5f6a3a4b
harshkumar1302 <bs824536@gmail.com>
2024-01-17 20:04:35 +0530

    Sum of 2 nums, Convert min to sec and Return the next num from the integer passed

--- veryEasyChallenges.js
+++ veryEasyChallenges.js
@@ -4,3 +4,38 @@ function intro() {
     return "hello edibat.com";
 }
 console.log(intro());
+
+
+/*   Challenge 01
+Return the Sum of Two Numbers
+Create a function that takes two numbers as arguments and returns their sum.
+*/
+
+function addition(a, b) {
+    return a + b;
+}
+console.log(addition(2, 9));
+
+
+/* Challenge 02
+Convert Minutes into Seconds
+Write a function that takes an integer minutes and converts it to seconds.
+*/
+
+function convertToSec(minutes) {
+    return minutes * 60;
+}
+
+console.log(convertToSec(5));
+
+
+/* Challenge 03
+Return the Next Number from the Integer Passed
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+*/
+
+function increments(number) {
+    return number = number + 1;
+}
+
+console.log(increments(34));
