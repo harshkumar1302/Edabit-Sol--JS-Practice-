f61b746c0a730eb6b1fd087bba6525c1d4f9955b
harshkumar1302 <bs824536@gmail.com>
2024-01-17 21:22:23 +0530

    Convert HOurs to sec, max edge of triangle, return the reminder, sum of polygon and basketball points

--- veryEasyChallenges.js
+++ veryEasyChallenges.js
@@ -97,3 +97,71 @@ var arr = [2, 5, 9, 7];
 console.log(`The first value in the given array is ${giveFirstValue(arr)}.`);
 
 
+/* Challeneg 07
+Convert Hours into Seconds
+Write a function that converts hours into seconds.
+*/
+
+function hoursToSec(hours) {
+    return hours * 3600;
+}
+
+var hours = 5;
+console.log(`${hours} hours = ${hoursToSec(hours)} seconds`);
+
+
+/* Challenge 08
+Maximum Edge of a Triangle
+Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+*/
+
+function nextEdge(side1, side2) {
+    if (side1 > 0 && side2 > 0) {
+        return (side1 + side2) - 1;
+    } else {
+        console.log("invalid numbers");
+    }
+}
+
+console.log(nextEdge(9, 2));
+
+
+/* Challenge 09
+Return the Remainder from Two Numbers
+There is a single operator in JavaScript, capable of providing the remainder of a division operation. 
+Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, 
+possibly zero. Return that value.
+*/
+
+function remainder(x, y) {
+    return x % y;
+}
+
+var x = 2;
+var y = 8;
+console.log(`The remainder of given parameters is ${remainder(x, y)}.`);
+
+/* Challenge 10
+Sum of Polygon Angles
+Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+*/
+
+function sumPolygon(n) {
+    return (n - 2) * 180;
+}
+var n = 4;
+console.log(`The sum of angles of ${n} sided polygon is ${sumPolygon(n)}.`);
+
+
+/* Challenge 11
+Basketball Points
+You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, 
+find the final points for the team and return that value.
+*/
+function points(twoPointers, threePointers) {
+    return (twoPointers * 2) + (threePointers * 3)
+}
+
+var twoPointers = 5;
+var threePointers = 2;
+console.log(`The score added by the 2 pointers and 3 pointers are ${points(twoPointers, threePointers)}.`);
